fa1dc4018886ac40d9969912d420c4a2a11915f2
Steve Bilogan <steve.bilogan@gmail.com>
2020-01-19 22:01:35 -0500

    android resources

---
+++ Cicerone.Android/SplashActivity.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading.Tasks;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Util;
+
+namespace Cicerone.Droid
+{
+    [Activity(Theme = "@style/MyTheme.Splash", MainLauncher = true, NoHistory = true)]
+    public class SplashActivity : AppCompatActivity
+    {
+        static readonly string TAG = "X:" + typeof(SplashActivity).Name;
+
+        public override void OnCreate(Bundle savedInstanceState, PersistableBundle persistentState)
+        {
+            base.OnCreate(savedInstanceState, persistentState);
+            Log.Debug(TAG, "SplashActivity.OnCreate");
+        }
+
+        public override void OnBackPressed() { }
+
+        // Launches the startup task
+        protected override void OnResume()
+        {
+            base.OnResume();
+            Task startupWork = new Task(() => { SimulateStartup(); });
+            startupWork.Start();
+        }
+
+        // Simulates background work that happens behind the splash screen
+        async void SimulateStartup()
+        {
+			//Startup work here
+            StartActivity(new Intent(Application.Context, typeof(RootActivity)));
+        }
+
+
+    }
+}
